165fa5ca9567d29ad9f058f5ba7f7dfb27570764
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-06-25 13:02:22 +0200

    feat:add exercise theMatchChecksOut

---
+++ theMatchChecksOut/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insert a number");
+string number1=Console.ReadLine();  
+double parsedNumber1=double.Parse(number1);
+Console.WriteLine("Insert a second number:");
+string number2=Console.ReadLine();
+double parsedNumber2=double.Parse(number2);
+double result=parsedNumber1+parsedNumber2;
+Console.WriteLine($"The result is :{parsedNumber1}+{parsedNumber2}={result}");
